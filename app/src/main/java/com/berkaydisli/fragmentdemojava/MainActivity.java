71972585f1d3f6b7b60b2118cb4f2a0ecc9668c1
berkaydisli <berkay.dsli@gmail.com>
2024-03-09 01:56:24 +0300

    Final

---
+++ app/src/main/java/com/berkaydisli/fragmentdemojava/MainActivity.java
@@ -0,0 +1,46 @@
+package com.berkaydisli.fragmentdemojava;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void goToFirst(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FirstFragment firstFragment = new FirstFragment();
+        //fragmentTransaction.add(R.id.frameLayout, firstFragment);
+        fragmentTransaction.replace(R.id.frameLayout, firstFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void goToSecond(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        SecondFragment secondFragment = new SecondFragment();
+        fragmentTransaction.replace(R.id.frameLayout, secondFragment);
+        fragmentTransaction.commit();
+    }
+}
